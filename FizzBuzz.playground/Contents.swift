0d0ae6fdb5276e835a14f7f109d4aeffba6b4c32
Batuhan <32312869+batuhankucukyildiz@users.noreply.github.com>
2023-09-04 14:55:11 +0300

    fizzbuzz

--- FizzBuzz.playground/Contents.swift
+++ FizzBuzz.playground/Contents.swift
@@ -1,3 +1,26 @@
-import UIKit
 
-var greeting = "Hello, playground"
+
+
+
+import Foundation
+
+
+var numbers : [Int] = []
+
+for i in 1...100 {
+    numbers.append(i)
+}
+
+for num in numbers{
+
+    if num % 15 == 0 {
+        print("\(num) fizzbuzz ")
+    }else if num % 3 == 0{
+        print("\(num) fizz")
+    }else if num % 5 == 0 {
+        print("\(num) buzz")
+
+    }else {
+        print(num)
+    }
+}
